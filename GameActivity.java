e978d908df71a6766a2c91f192155a8549d643d4
tin villanueva <mcpvillanueva@gmail.com>
2015-05-07 22:53:27 +0800

    MainMenu & game_screen working

--- GameActivity.java
+++ GameActivity.java
@@ -1,51 +1,86 @@
 package com.tinvillanueva.martianlander;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
+public class GameActivity extends Activity implements View.OnTouchListener {
 
-public class GameActivity extends ActionBarActivity implements View.OnTouchListener {
+    private GameView gameView;
+    private ImageButton leftThruster;
+    private ImageButton rightThruster;
+    private ImageButton mainThruster;
+    private ImageButton pause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  //removes title bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.game_activity);
+        setContentView(R.layout.game_screen);
+
+//        Display display = getWindowManager().getDefaultDisplay();
+//        DisplayMetrics outMetrics = new DisplayMetrics();
+//        display.getMetrics(outMetrics);
+
+        gameView = (GameView) findViewById(R.id.gameView);
+
+        leftThruster = (ImageButton) findViewById(R.id.btnLeftThruster);
+        leftThruster.setOnTouchListener(this);
+
+        rightThruster = (ImageButton) findViewById(R.id.btnRightThruster);
+        rightThruster.setOnTouchListener(this);
+
+        mainThruster = (ImageButton) findViewById(R.id.btnMainThruster);
+        mainThruster.setOnTouchListener(this);
+
+        pause = (ImageButton) findViewById(R.id.btnPause);
+        pause.setOnTouchListener(this);
+
     }
 
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+
+                break;
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.startGame();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pauseGame();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent event) {
-        return false;
     }
 }
